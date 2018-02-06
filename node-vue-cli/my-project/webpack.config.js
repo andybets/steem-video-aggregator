46a648232dfba5953990e76ec0b88fb65d90739e
Andy Betsworth <andyb@datadevil.co.uk>
2018-02-06 15:48:22 +0000

    Removed UglifyJS Due to Build Error (temporary)

--- node-vue-cli/my-project/webpack.config.js
+++ node-vue-cli/my-project/webpack.config.js
@@ -66,12 +66,14 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
+/*
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
         warnings: false
       }
     }),
+*/
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
